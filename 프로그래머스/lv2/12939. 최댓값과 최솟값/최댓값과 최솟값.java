2b7ddea15877eaa37761d855b2a4ce6504eb240f
whdydrhks <109534450+whdydrhks@users.noreply.github.com>
2023-04-06 00:05:51 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.57 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,18 +1,29 @@
 import java.util.*;
 class Solution {
-    public String solution(String s) {
-        String answer = "";
-        String[] strarr = s.split(" ");
-        int[] arr = new int[strarr.length];
-        for(int i=0; i<arr.length; i++){
-            arr[i] = Integer.parseInt(strarr[i]);
-        }
-        Arrays.sort(arr);
-        answer += arr[0];
-        answer+=" ";
-        answer+=arr[arr.length-1];
+    public String solution(String str) {
+//         String answer = "";
+//         String[] strarr = s.split(" ");
+//         int[] arr = new int[strarr.length];
+//         for(int i=0; i<arr.length; i++){
+//             arr[i] = Integer.parseInt(strarr[i]);
+//         }
+//         Arrays.sort(arr);
+//         answer += arr[0];
+//         answer+=" ";
+//         answer+=arr[arr.length-1];
         
-        System.out.println(answer);
-        return answer;
+//         System.out.println(answer);
+//         return answer;
+        
+        String[] tmp = str.split(" ");
+        int min, max, n;
+        min = max = Integer.parseInt(tmp[0]);
+        for (int i = 1; i < tmp.length; i++) {
+                n = Integer.parseInt(tmp[i]);
+            if(min > n) min = n;
+            if(max < n) max = n;
+        }
+
+        return min + " " + max;
     }
 }
