45e4ad4a35481e76045b92a6615cd402bddb8bce
whdydrhks <109534450+whdydrhks@users.noreply.github.com>
2023-04-12 22:31:48 +0900

    [level 2] Title: 올바른 괄호, Time: 19.07 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,22 @@
+import java.util.*;
+
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        int sum=0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='(') sum++;
-            else sum--;
-            
-            if(sum<0) {
-                answer=false;
-                break;
-            }
-        }
-        if(sum!=0) answer=false;
-
-        return answer;
+		Stack<Character> stack = new Stack<>();
+		for(int i=0; i<s.length(); i++) {
+			char temp = s.charAt(i);
+			
+			if(temp==')' && stack.size()==0) 
+				return false;
+			else if(temp==')' && stack.size()>0) 
+				stack.pop();
+			else if(temp=='(') {
+				stack.push(temp);
+			}
+		}
+		
+		if(stack.size()!=0) answer=false;
+		return answer;
     }
 }
