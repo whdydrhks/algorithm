3c00b231ac87fb20a5dead729c48fd6fdde2ec86
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-06-17 22:36:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.01 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,11 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for(int i=0; i<completion.length; i++){
-            if(!participant[i].equals(completion[i])) return participant[i];
+        HashMap<String, Integer> hmap = new HashMap<>();
+        for(String parti : participant) {
+            hmap.put(parti, hmap.getOrDefault(parti, 0)+1); // 입력한 데이터의 중복을 고려하여 카운트.
+        } // ex) {"a"=1, "b"=3, "c"=1}
+        
+        for(String comple : completion) {
+            hmap.put(comple, hmap.get(comple)-1); // 중복된 이름이 있어도 value의 값-1로 갱신된다. 
+        } 
+        
+        for(String key : hmap.keySet()) {
+            if(hmap.get(key)>=1) return key;
         }
-        return participant[participant.length-1];
+        return "";
     }
 }
