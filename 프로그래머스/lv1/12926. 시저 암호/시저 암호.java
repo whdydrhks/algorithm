c1c64e1cb3b2bf40a2b76dfb5ae711ca404755fa
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-06-28 02:16:25 +0900

    [level 1] Title: 시저 암호, Time: 79.28 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -1,29 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        String[] arr = s.split("");
-        for(int i=0; i<arr.length; i++){
-            String tmp = check(arr[i], n);
-            answer+=tmp;
+        String result = "";
+        
+        n = n % 26;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(ch)) {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+            result += ch;
         }
-        return answer;
-    }
-    
-    public static String check(String target, int n) {
-        String tmp="";
-        int sum=0;
-        System.out.println("origin : "+target);
-        if(target.equals(" ")) {
-            return " ";
-        }
-        else {
-            // a:97, z:122 | A:65, Z:90
-            sum = target.charAt(0)+n;
-            if(target.charAt(0)>='a' && target.charAt(0)<='z' && sum>122) sum = sum-26;
-            else if(target.charAt(0)>='A' && target.charAt(0)<='Z' && sum>90) sum = sum-26;
-            
-            return tmp+(char)sum;
-        }
+        return result;
     }
 }
