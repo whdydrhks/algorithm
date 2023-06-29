969faee0a21f1931af6da5bc28d6b3eb0e1dc929
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-06-30 01:18:32 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.61 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,7 +1,8 @@
+// A 배열을 오름차순으로 정렬하고,
+// B 배열을 내림차순으로 정렬해서 sum을 최솟값으로 맞춘다.
 import java.util.*;
 class Solution
 {
-    static int idx;
     public int solution(int []A, int []B)
     {
         Arrays.sort(A);
@@ -10,35 +11,10 @@ class Solution
             tmp[i]=B[i];
         }
         Arrays.sort(tmp, Collections.reverseOrder());
-        System.out.println(Arrays.toString(tmp));
         int sum = 0;
         for(int i=0; i<tmp.length; i++){
             sum+=A[i]*tmp[i];
         }
-//         int idx = 0;
-//         int min = Integer.MAX_VALUE;
-        
-//         for(int i=0; i<B.length; i++){
-//             min = Math.min(min, check(A, B, idx++));
-//         }
-        
         return sum;
     }
-    
-//     public static int check(int[] A, int[] B, int idx) {
-//         int sum=0;
-//         int[] tmp = new int[B.length];
-        
-//         for(int i=idx, count=0; count<B.length; i++, count++){
-//             if(i==B.length) i=0;
-//             tmp[count] = B[i];
-//         }
-//         System.out.println("************");
-//         System.out.println(Arrays.toString(tmp));
-//         for(int i=0; i<A.length; i++){
-//             sum+=A[i]*tmp[i];
-//         }
-        
-//         return sum;
-//     }
 }
