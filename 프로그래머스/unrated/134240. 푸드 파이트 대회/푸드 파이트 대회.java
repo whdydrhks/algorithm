bfff24a6c9e9f6c7a8524841afbdc8bceeb232ed
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-07-04 19:49:58 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 16.80 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,19 +1,32 @@
+// class Solution {
+//     public String solution(int[] food) {
+//         String front = "";
+//         int idx=food.length/2;
+        
+//         for(int i=1; i<food.length; i++){
+//             int val=food[i]/2;
+//             for(int j=0; j<val; j++){
+//                 front+=i;
+//             }
+//         }
+//         String back="";
+//         for(int i=front.length()-1; i>=0; i--) {
+//             back+=front.charAt(i);
+//         }
+        
+//         return front+"0"+back;
+//     }
+// }
 class Solution {
     public String solution(int[] food) {
-        String front = "";
-        int idx=food.length/2;
-        
-        for(int i=1; i<food.length; i++){
-            int val=food[i]/2;
-            for(int j=0; j<val; j++){
-                front+=i;
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        String back="";
-        for(int i=front.length()-1; i>=0; i--) {
-            back+=front.charAt(i);
-        }
-        
-        return front+"0"+back;
+
+        return answer;
     }
 }
