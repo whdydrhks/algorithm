cc0a05316ee8b9ee1020a6f5e6e047ff8dfab15a
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-07-21 10:07:13 +0900

    [level 2] Title: [1차] 캐시, Time: 2.42 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        
+        ArrayList<String> list = new ArrayList<>();
+        if(cacheSize==0) return cities.length*5;
+        
+        for(int i=0; i<cities.length; i++){
+            String city = cities[i].toUpperCase();
+            
+            if(list.remove(city)) {
+                list.remove(city);
+                list.add(city);
+                answer+=1;
+            }
+            else {
+                if(list.size() < cacheSize) {
+                    list.add(city);
+                    answer+=5;
+                    continue;
+                }
+                list.remove(0);
+                list.add(city);
+                answer+=5;
+            }
+        }
+        
+        return answer;
+    }
+    
+    
+}
