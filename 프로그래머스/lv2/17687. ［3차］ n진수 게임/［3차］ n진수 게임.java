1ac176ee7466b58d404c5e04e7ee202614385ad4
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-08-03 15:13:16 +0900

    [level 2] Title: [3차] n진수 게임, Time: 5.39 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        
+        int order=0;
+        for(int i=0; i<t*m; i++){
+            String number = Integer.toString(i, n);
+            
+            if(number.length()==1) { // 한문자
+                order++;
+                if(order==m+1) order=1;
+                if(order==p) { // 내 차례
+                    answer = answer + number.toUpperCase();
+                    // answer+=number;
+                    if(answer.length() == t) return answer;
+                }
+                else {
+                    continue;
+                }
+            }
+            else { // 문자열
+                for(int j=0; j<number.length(); j++){
+                    order++;
+                    if(order==m+1) order=1;
+                    if(order==p) {
+                        answer = answer+String.valueOf(number.charAt(j)).toUpperCase();
+                        // answer+=number.charAt(j);   
+                        if(answer.length() == t) return answer;
+                    }
+                    else {  
+                        continue;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
