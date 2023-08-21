77a25d68f140aa9a35509ad9217e4cd4b683df01
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-08-21 09:34:57 +0900

    [level 2] Title: 모음 사전, Time: 0.26 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,34 +1,7 @@
-import java.util.*;
 class Solution {
-    static String[] strarr;
-    static String[] arr = {"A","E","I","O","U"};
-    static ArrayList<String> list;
     public int solution(String word) {
-        int answer = 0;
-        
-        list = new ArrayList<>();
-        for(int i=1; i<=5; i++){
-            strarr = new String[i];
-            perm2(0,i);
-        }
-        Collections.sort(list);
-        answer = list.indexOf(word)+1;
+        int answer = 0, per = 3905;
+        for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
         return answer;
-    }
-    
-    public static void perm2(int depth,int limit) {
-        if(depth==limit) {
-            String tmp="";
-            for(int i=0; i<strarr.length; i++){
-                tmp+=strarr[i];
-            }
-            list.add(tmp);
-            return;
-        }
-        
-        for(int i=0; i<5; i++){
-            strarr[depth] = arr[i];
-            perm2(depth+1, limit);
-        }
     }
 }
