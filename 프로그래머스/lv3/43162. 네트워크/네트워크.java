20b362c0b56cb968c6c92a6bf604f004a0208e73
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-08-29 10:23:12 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,38 +1,21 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[][] visited = new boolean[n][n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(computers[i][j]==1 && !visited[i][j]) {
-                    bfs(i,j,visited,computers,n);
-                    answer++;
-                }
+        boolean[] chk = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(!chk[i]) {
+                dfs(computers, chk, i);
+                answer++;
             }
         }
         return answer;
     }
-    
-    public static void bfs(int x, int y, boolean[][] visited, int[][] computers,int n) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        visited[x][y]=true;
-        queue.offer(new int[] {x,y});
-        
-        while(!queue.isEmpty()) {
-            int[] xy = queue.poll();
-            
-            for(int i=0; i<n; i++){
-                if(computers[xy[0]][i]==1 && !visited[xy[0]][i]) {
-                    queue.offer(new int[] {xy[0],i});
-                    visited[xy[0]][i]=true;
-                }
-                if(computers[i][xy[1]]==1 && !visited[i][xy[1]]) {
-                    queue.offer(new int[] {i, xy[1]});
-                    visited[i][xy[1]]=true;
-                }
+    void dfs(int[][] computers, boolean[] chk, int start) {
+        chk[start] = true;
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[start][i] == 1 && !chk[i]) {
+                dfs(computers, chk, i);
             }
         }
-        return;
     }
 }
