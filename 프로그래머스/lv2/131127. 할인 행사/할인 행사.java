758331cd6454752901be16c67577d3a19ae2b0d1
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-09-08 11:36:08 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -1,35 +1,28 @@
 import java.util.*;
-class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        int sum=0;
-        for(int num:number) sum+=num;
-        HashMap<String, Integer> wantHmap = new HashMap<>();
-        for(int i=0; i<want.length; i++){
-            wantHmap.put(want[i], number[i]);
-        }
-        
-        for(int i=0; i<=discount.length-sum; i++){
-            HashMap<String, Integer> hmap = new HashMap<>();
-            for(int j=i; j<i+sum; j++){
-                hmap.put(discount[j], hmap.getOrDefault(discount[j], 0)+1);
-            } 
-            
-            if(check(wantHmap, hmap)) answer++;
-        }
-    
-        return answer;
-    }
-    
-    public static boolean check(HashMap<String, Integer> wantHmap, HashMap<String, Integer> hmap) {
-        for(String key : hmap.keySet()) {
-            if(hmap.get(key) != wantHmap.get(key)) return false;
-        }
-        return true;
-    }
-}
 
-// want의 sum(number) 만큼 discount 0부터 검색을 한다.
-    // 길이가 sum(number)보다 짧으면 안됨
-// discount를 돌면서 제품에 대한 hmap으로 이름:갯수를 파악
-    // 길이 sum(number)가 되었을 때, want의 제품과 수량이 일치하는지 비교.
+class Solution {
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            int left = 0;
+            int right = 9;
+            Map<String, Integer> map = new HashMap<>();
+            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
+            while(right < discount.length){
+                if(check(map,want,number)) answer++;
+
+                right++;
+                if(right < discount.length)
+                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
+                map.put(discount[left],map.get(discount[left])-1);
+                left++;
+            }
+
+            return answer;
+        }
+        private boolean check(Map<String,Integer> map,String[] want,int[] number){
+            for(int i =0;i<want.length;i++){
+                if(map.getOrDefault(want[i],0) != number[i]) return false;
+            }
+            return true;
+        }
+    }
