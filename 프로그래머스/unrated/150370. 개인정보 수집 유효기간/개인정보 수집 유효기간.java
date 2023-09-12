a66876569f14243563c9483e18f3ad8557d947d1
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-09-12 12:22:22 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.33 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,48 +1,35 @@
-import java.util.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
-        HashMap<String, Integer> hmap = new HashMap<>();
-        ArrayList<Integer> list = new ArrayList<>();
-        for(String term : terms) {
-            String[] strArr = term.split(" ");
-            hmap.put(strArr[0], Integer.parseInt(strArr[1]));
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> termMap = new HashMap<>();
+        int date = getDate(today);
+
+        for (String s : terms) {
+            String[] term = s.split(" ");
+
+            termMap.put(term[0], Integer.parseInt(term[1]));
         }
-        int idx=1;
-        int count=0;
-        for(String privacy : privacies) {
-            String[] strArr = privacy.split(" ");
-            if(check(today, strArr[0], strArr[1], hmap)) {
-                idx++;
-                continue;
+        for (int i = 0; i < privacies.length; i++) {
+            String[] privacy = privacies[i].split(" ");
+
+            if (getDate(privacy[0]) + (termMap.get(privacy[1]) * 28) <= date) {
+                answer.add(i + 1);
             }
-            list.add(idx++);
         }
-        
-        answer = new int[list.size()];
-        for(int i=0; i<answer.length; i++){
-            answer[i]=list.get(i);
-        }
-        
-        return answer;
+        return answer.stream().mapToInt(integer -> integer).toArray();
     }
-    
-    public static boolean check(String today, String start, String type, HashMap<String, Integer> hmap) {
-        LocalDate todayDate = LocalDate.parse(today, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        LocalDate startDate = LocalDate.parse(start, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        LocalDate endDate = LocalDate.parse(start, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        endDate = startDate.plusMonths(hmap.get(type));
-        
-        if(todayDate.isAfter(startDate) && todayDate.isBefore(endDate)) return true;
-        return false;        
+
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
     }
 }
-
-// 해결과정
-// 1. terms를 hmap에 넣는다. <String, Integer>
-// 2. privacies의 날짜를 약관에 맞춰 증가한다. 
-//  2-1. "오늘" 날짜가 p의 "시작일"과 "종료일" 사이이면 check
