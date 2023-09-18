822568762244dd38f0cf285b917f519397aafde1
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-09-18 17:00:46 +0900

    [unrated] Title: 귤 고르기, Time: 69.06 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -1,32 +1,25 @@
 import java.util.*;
+
 class Solution {
-    
-    static ArrayList<Integer> list;
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        HashMap<Integer, Integer> hmap = new HashMap<>(); // 종류, 개수
-        for(int num : tangerine) {
-            hmap.put(num, hmap.getOrDefault(num,0)+1);
+        HashMap<Integer,Integer> map =new HashMap<>();
+
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
-        
-        list = new ArrayList<>();
-        for(int num : hmap.values()) {
-            list.add(num);
-        }
-        Collections.sort(list, Collections.reverseOrder());
-        
-        int sum=0;
-        for(Integer num : list) {
-            sum+=num;
-            if(sum>=k) return answer+1;
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2)-map.get(o1));
+
+        for(Integer key:list){
+            k -=map.get(key);
             answer++;
+            if(k<=0){
+                break;
+            }
         }
-        
-        
-        return 1;
+
+        return answer;
     }
 }
-
-// 리스트에 귤 개수를 담고,
-// 역순으로 정렬
-// 재귀를 돌면서 k가 되는 경우 찾기 depth:1~list.size()
