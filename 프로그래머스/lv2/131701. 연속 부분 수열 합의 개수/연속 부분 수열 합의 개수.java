e728f863f97324e8e6473ed1c018629e0b41b892
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-09-22 09:59:08 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 81.79 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,35 +1,15 @@
 import java.util.*;
+
 class Solution {
-    static HashSet<Integer> hset;
-    public int solution(int[] elements) {
-        int[] newElements = new int[elements.length*2];
-        for(int i=0, idx=0; i<newElements.length; i++, idx++){
-            if(idx==elements.length) idx=0;
-            newElements[i] = elements[idx];
-        }
-        
-        hset = new HashSet<>();
-        for(int element : elements) hset.add(element);
-        
-        for(int i=2; i<=elements.length; i++){
-            check(i, newElements, elements.length-i+1);
-        }
-        return hset.size();
-    }
-    
-    public static void check(int len, int[] newElements, int size) {
-        int totalCount=0;
-        for(int i=0; i<newElements.length; i++){
-            if(totalCount==size) break;
-            int sum=0, count=0;
-            for(int start=i; start<newElements.length; start++){
-                if(count==len) {
-                    hset.add(sum);
-                    break;
+        public int solution(int[] elements) {
+            Set<Integer> set = new HashSet<>();
+            int[] dp = new int[elements.length];
+            for(int len = 1;len <= elements.length; len++){
+                for(int i = 0;i<elements.length;i++){
+                    dp[i] += elements[(len+i-1)%elements.length];
+                    set.add(dp[i]);
                 }
-                sum+=newElements[start];
-                count++;
             }
+            return set.size();
         }
     }
-}
