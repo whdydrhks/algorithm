e6c5863e1dbab462bf13380e9e2aed944d39c667
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-09-25 13:51:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i=0; i<absolutes.length; i++){
+            if(signs[i]) answer = answer+absolutes[i];
+            else answer = answer - absolutes[i];
+        }
+        
+        return answer;
+    }
+}
