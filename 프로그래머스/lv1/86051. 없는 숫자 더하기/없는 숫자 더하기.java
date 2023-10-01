ff2d48d6132bc0137e36836fe4768c5d6142ac84
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-10-01 15:30:56 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for(int i=0; i<=9; i++){
+            answer+=i;
+        }
+        for(int i=0; i<numbers.length; i++){
+            answer -= numbers[i];
+        }
+        return answer;
+    }
+}
