bf3acae972c4b8da47b0a8fb307c3e5d03f0855b
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-10-04 14:21:50 +0900

    [level 2] Title: 구명보트, Time: 9.15 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -1,29 +1,13 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         Arrays.sort(people);
-        
-        // 10 10 60 70 80  100 => 3
-        int idx=0;
-        for(int i=people.length-1; i>=0; i--){
-            if(people[i]==0) break;
-            int sum=people[i];
-            
-            boolean flag=false;
-            for(int j=idx; j<i; j++) {
-                if(people[j]==0) continue;
-                sum+=people[j];
-                if(sum>limit) break;
-                people[j]=0;
-                flag=true;
-                break;
-            }
-            answer++;
-            if(flag) idx++;
-            // System.out.println(list.toString());
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
-        
-        return answer;
+        return people.length - i;
     }
 }
