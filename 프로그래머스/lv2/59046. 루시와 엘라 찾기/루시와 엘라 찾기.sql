002358913e7d4f475aa061c412b655a30af0eb6b
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-10-06 09:22:16 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,8 @@
+SELECT
+    INS.ANIMAL_ID,
+    INS.NAME,
+    INS.SEX_UPON_INTAKE
+FROM    
+    ANIMAL_INS AS INS
+WHERE
+    NAME='Lucy' or NAME='Ella' or NAME='Pickle' or NAME='Rogan' or NAME='Sabrina' or NAME='Mitty'
