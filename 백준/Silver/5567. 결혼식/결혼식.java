f8ece1cbb1cd139a112991b29a4252773ca6cc2b
조용관 <109534450+whdydrhks@users.noreply.github.com>
2023-10-14 16:24:27 +0900

    [Silver II] Title: 결혼식, Time: 132 ms, Memory: 16624 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -8,35 +8,46 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
-		V = Integer.parseInt(br.readLine());
-		friends_list = new List[V+1];
-		visited = new boolean[V+1];
-		for(int i=0; i<=V; i++) friends_list[i] = new ArrayList<>();
-		
-		E = Integer.parseInt(br.readLine());
-		for(int i=0; i<E; i++) {
+		StringTokenizer st;
+		int res = 0;
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		ArrayList<Integer>[] f = new ArrayList[n];
+		for (int i = 0; i < n; i++) {
+			f[i] = new ArrayList<Integer>();
+		}
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			friends_list[from].add(to);
-			friends_list[to].add(from);
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			f[a].add(b);
+			f[b].add(a);
 		}
-		
-		dfs(1, 0);
-		int ans=0;
-		for(boolean v:visited) if(v) ans++;
-		System.out.println(ans==0 ? 0 : ans-1);
-		
-	}
-	
-	public static void dfs(int start, int depth) {
-		if(depth==2) return; // 0:친구, 1:친구의 친구
-		
-		for(int friend : friends_list[start]) {
-			visited[friend]=true;
-			dfs(friend, depth+1);
+
+		boolean[] v = new boolean[n];
+
+		v[0] = true;
+
+		Queue<Integer> q = new LinkedList<Integer>();
+
+		q.add(0);
+		int cnt = 0;
+		while (!q.isEmpty()) {
+			int size = q.size();
+			if(cnt==2) break;
+			for (int i = 0; i < size; i++) {
+				int x = q.poll();
+				for (int nowx : f[x]) {
+					if (v[nowx])
+						continue;
+					v[nowx] = true;
+					res++;
+					q.add(nowx);
+				}
+			}
+			cnt++;
 		}
+		System.out.println(res);
 	}
 }
