5098fd7c3a0de49eb85be0b846755e0f10619e6f
조용관 <109534450+whdydrhks@users.noreply.github.com>
2024-05-12 16:25:08 +0900

    [level 1] Title: 시저 암호, Time: 32.57 ms, Memory: 93.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(String s, int n) {
+        String result = "";
+        
+        // n = n % 26;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            } else if (Character.isUpperCase(ch)) {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+            result += ch;
+        }
+        return result;
+    }
+}
